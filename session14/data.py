2e02d71a1b1e43968035b53ea983d0126f640fee
Nguyễn Bá Hùng <hung.nb216835@sis.hust.edu.vn>
2025-05-22 20:52:05 +0700

    push ảnh

--- session14/data.py
+++ session14/data.py
@@ -1,13 +1,13 @@
 from sklearn.datasets import make_blobs , make_moons
 import matplotlib.pyplot as plt
 
-x1,labels1 = make_blobs(n_samples=100, cluster_std=1.0 , centers=3, random_state=42)
+x1,labels1 = make_blobs(n_samples=200, cluster_std=1.0 , centers=3, random_state=42)
 plt.scatter(x1[:,0], x1[:,1], c=labels1)
 plt.title("make_blobs")
 plt.savefig("session14/make_blobs.png")
 plt.clf()
 
-x2,labels2 = make_moons(n_samples=100, noise=0.1)
+x2,labels2 = make_moons(n_samples=200, noise=0.1, random_state=42) 
 plt.scatter(x2[:,0], x2[:,1], c=labels2)
 plt.title("make_moons")
 plt.savefig("session14/make_moons.png")
